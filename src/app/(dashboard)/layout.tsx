fecad5a7cbafef7d77f767ff463ad2cd49eae423
Faizo Kigenyi <faizokigenyi@gmail.com>
2025-03-17 01:45:58 -0500

    Add contents to Menu, NavBar and configure tailwindCSS files

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,7 @@
 import Menu from "../components/Menu";
 import Image from "next/image";
 import Link from "next/link";
-import NavBar from "../components/NavBar";
+import Navbar from "../components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -23,7 +23,7 @@ export default function DashboardLayout({
       </div>
       {/* RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg=[#F7F8FA] overflow-scroll flex flex-col">
-        <NavBar />
+        <Navbar />
         {children}
       </div>
     </div>
