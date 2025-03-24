1bf691363334189ce731ce7ba998dc4b08c53496
Faizo Kigenyi <faizokigenyi@gmail.com>
2025-03-24 02:15:09 -0500

    Update lesson page

--- src/app/(dashboard)/list/assignments/page.tsx
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -4,7 +4,6 @@ import Table from "@/app/components/Table";
 import TableSearch from "@/app/components/TableSearch";
 import { assignmentsData, role } from "@/lib/data";
 import Image from "next/image";
-import Link from "next/link";
 
 type Assignment = {
   id: number;
@@ -39,7 +38,7 @@ const columns = [
   },
 ];
 
-export default function AssignmentListPage() {
+const AssignmentListPage = () => {
   const renderRow = (item: Assignment) => (
     <tr
       key={item.id}
@@ -65,10 +64,12 @@ export default function AssignmentListPage() {
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
-      {/** TOP*/}
+      {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Subjects</h1>
-        <div className="flex flex-col md:flex-row items-center gap-4 ">
+        <h1 className="hidden md:block text-lg font-semibold">
+          All Assignments
+        </h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
@@ -77,7 +78,6 @@ export default function AssignmentListPage() {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-
             {role === "admin" ||
               (role === "teacher" && (
                 <FormModal table="assignment" type="create" />
@@ -85,10 +85,12 @@ export default function AssignmentListPage() {
           </div>
         </div>
       </div>
-      {/** lIST*/}
+      {/* LIST */}
       <Table columns={columns} renderRow={renderRow} data={assignmentsData} />
-      {/** PAGINATION*/}
+      {/* PAGINATION */}
       <Pagination />
     </div>
   );
-}
+};
+
+export default AssignmentListPage;
